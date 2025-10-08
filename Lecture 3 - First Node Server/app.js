f0c764542ef1ac6e4a6c9b99d8d7d6465a313f01
raju15022004 <raju032004@gmail.com>
2025-10-08 10:34:06 +0530

    app.js

--- Lecture 3 - First Node Server/app.js
+++ Lecture 3 - First Node Server/app.js
@@ -1,7 +1,15 @@
 const http = require('http');
 
 const server = http.createServer((req,res)=>{
-  console.log(req);
+  console.log(req.url,req.method,req.headers);
+  res.setHeader('Content-Type','text/html');
+  res.write('<html>');
+  res.write('<head><title>Complete Coding<title></head>');
+  res.write('<body><h1>Like / share / Subscribe</h1></body>');
+  res.write('</html>');
+  res.end();
+  // process.exit();
+
 });
 
 const PORT=3001;
