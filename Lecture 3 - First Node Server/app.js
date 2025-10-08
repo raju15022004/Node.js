e2c85282987d7bed1251f5e3b22477c26d183ee3
raju15022004 <raju032004@gmail.com>
2025-10-08 22:01:12 +0530

    app.js

--- Lecture 3 - First Node Server/app.js
+++ Lecture 3 - First Node Server/app.js
@@ -1,14 +1,24 @@
 const http = require('http');
 
 const server = http.createServer((req,res)=>{
-  console.log(req.url,req.method,req.headers);
+
   res.setHeader('Content-Type','text/html');
   res.write('<html>');
-  res.write('<head><title>Complete Coding<title></head>');
-  res.write('<body><h1>Like / share / Subscribe</h1></body>');
+  res.write('<head><title>Complete Coding</title></head>');
+
+  if(req.url === '/'){
+    res.write('<h1>Welcome to Home page</h1>');
+    res.end();
+  }
+  else if(req.url.toLowerCase() === '/products'){
+    res.write('<h1>Product</h1>');
+    res.end();
+  }
+
+  res.write('<body><h1> Like / Share / Subscribe<h1></body>');
   res.write('</html>');
   res.end();
-  // process.exit();
+  // console.log(req.url,req.method,req.headers);
 
 });
 
