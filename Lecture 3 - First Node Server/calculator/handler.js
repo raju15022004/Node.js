238254cb04821f323738988382fb1c1ffab0a2f6
raju15022004 <raju032004@gmail.com>
2025-10-11 13:35:53 +0530

    handler.js

--- Lecture 3 - First Node Server/calculator/handler.js
+++ Lecture 3 - First Node Server/calculator/handler.js
@@ -1,5 +1,52 @@
-const requestHandler=(req,res) =>{
+const {sumRequestHandler}= require('./sum');
 
+const requestHandler=(req,res) =>{
+  console.log(req.url,req.method);
+  if(req.url === '/'){
+    res.setHeader('Content-Type','text/html');
+    res.write(`
+      <html>
+      <head><title>Practise Set </title></head>
+      <body>
+      <h1>Welcome to Calculator</h1>
+      <a href="/calculator">GO To Calculator</a>
+      </body>
+      </html>
+      `);
+      return res.end();
+  }
+  else if (req.url.toLowerCase() === "/calculator"){
+    res.setHeader('Content-Type','text/html');
+    res.write(`
+      <html>
+      <head><title>Practise Set </title></head>
+      <body>
+      <h1>Here is the Calculator</h1>
+      <from action="/calculate-result" method="POST">
+      <input type="text" placeholder="First Num" name="first"/>
+      <input type="text" placeholder="Second Num" name="second"/>
+      <input type="submit" value="Sum">
+      </form>
+      </body>
+      </html>
+      `);
+      return res.end();
+  }
+  else if(req.url.toLowerCase()==="/calculate-result" && req.method === 'POST'){
+    sumRequestHandler(req,res);
+  }
+
+  res.setHeader('Content-Type','text/html');
+    res.write(`
+      <html>
+      <head><title>page Does not Exist </title></head>
+      <body>
+      <h1>Welcome to Calculator</h1>
+      <a href="/">GO To Home</a>
+      </body>
+      </html>
+      `);
+      return res.end();
 }
 
 exports.requestHandler=requestHandler;
