f108c4e9bc5fc521169b78181808103c69a39d65
raju15022004 <raju032004@gmail.com>
2025-10-22 19:02:18 +0530

    user.js

---
+++ lecture 9/app.js
@@ -0,0 +1,11 @@
+const { Console } = require('console');
+const { copyFileSync } = require('fs');
+const http = require('http');
+const requestHandler=require('./user')
+
+const server = http.createServer(requestHandler);
+
+const PORT = 3002;
+server.listen(PORT,()=>{
+  console.log(`Server running on address http://localhost:${PORT}`);
+});
