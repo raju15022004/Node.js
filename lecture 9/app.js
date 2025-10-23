1e9f190d1685d32b195a0d0781e996928cd2d6e4
raju15022004 <raju032004@gmail.com>
2025-10-23 22:51:56 +0530

    app.js

--- lecture 9/app.js
+++ lecture 9/app.js
@@ -4,7 +4,7 @@ const server = http.createServer((req,res)=>{
   console.log(req);
 });
 
-const PORT = 3001;
+const PORT = 3002;
 server.listen(PORT,()=>{
   console.log(`Server running on address http://localhost:${PORT}`);
 });
