eeadd734dfa36e4b1a45047cdf88415e05a8266d
raju15022004 <raju032004@gmail.com>
2025-10-27 00:01:13 +0530

    app.js

--- lecture 9/app.js
+++ lecture 9/app.js
@@ -1,10 +1,26 @@
 const http = require('http');
+const express=require('express');
+const requestHandler = require('./user'); // Import your custom handler
 
-const server = http.createServer((req,res)=>{
-  console.log(req);
+
+// ✅ Correctly pass the handler function to createServer
+// const server = http.createServer(requestHandler);
+
+const app=express();
+
+app.use((req,res,next)=>{
+  console.log("Come in first middleware",req.url,req.method);
+  next();
 });
 
-const PORT = 3001;
-server.listen(PORT,()=>{
+app.use((req,res,next)=>{
+  console.log("Come in second middleware",req.url,req.method);
+  res.send("<p>Welcome to complete Coding Nodejs series</p>");
+});
+
+const server=http.createServer(app);
+
+const PORT = 3002;
+server.listen(PORT, () => {
   console.log(`Server running on address http://localhost:${PORT}`);
 });
--- lecture 9/user.js
+++ lecture 9/user.js
@@ -1,25 +1,46 @@
-const http = require('http');
+// user.js
+const fs = require('fs');
 
-const server = http.createServer((req,res)=>{
-res.write('<html>');
-res.write('<head><title>Complete Coding</title></head>');
-res.write('<body><h1>Enter Your Details:</h1>');
-res.write('<from action="/submit-details" method="POST">');
-res.write('<input type="text" name="username" placeholder="Enter your name"><br>');
-res.write('<label for="male">male</label>')
-res.write('<input type="radio" id="male" name="gender" value="male" />')
-res.write('<lable for="female">Female</lable>')
-res.write('<input type="radio" id="female" name="gender" value="female" />')
-res.write('<input type="submit" value="Submit">');
-res.write('</form>');
+const requestHandler = (req, res) => {
+  console.log(req.url, req.method);
 
-res.write('</body>');
-res.write('</html>');
-return res.end();
+  if (req.url === '/' && req.method === 'GET') {
+    res.write('<html>');
+    res.write('<head><title>Complete Coding</title></head>');
+    res.write('<body><h1>Enter Your Details:</h1>');
+    res.write('<form action="/submit-details" method="POST">');
+    res.write('<input type="text" name="username" placeholder="Enter your name"><br><br>');
+    res.write('<label for="male">Male</label>');
+    res.write('<input type="radio" id="male" name="gender" value="male"><br>');
+    res.write('<label for="female">Female</label>');
+    res.write('<input type="radio" id="female" name="gender" value="female"><br><br>');
+    res.write('<input type="submit" value="Submit">');
+    res.write('</form>');
+    res.write('</body>');
+    res.write('</html>');
+    return res.end();
+  }
 
-});
+  if (req.url === '/submit-details' && req.method === 'POST') {
+    const body = [];
 
-const PORT=300;
-server.listen(PORT,()=>{
-  console.log(`Server running on address http://localhost:${PORT}`);
-});
+    req.on('data', chunk => body.push(chunk));
+
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log('Form Data:', parsedBody);
+      fs.writeFileSync('details.txt', parsedBody);
+
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.write('<html>');
+      res.write('<head><title>Submitted</title></head>');
+      res.write('<body><h1>Details saved successfully!</h1>');
+      res.write('<a href="/">Go Back</a>');
+      res.write('</body>');
+      res.write('</html>');
+      return res.end();
+    });
+  }
+};
+
+module.exports = requestHandler;
