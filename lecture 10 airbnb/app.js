c1c9238c91d9db2d743dfd3d047faf1eb5fc4c20
raju15022004 <raju032004@gmail.com>
2025-11-02 22:30:53 +0530

    hostRouter.js

--- lecture 10 airbnb/app.js
+++ lecture 10 airbnb/app.js
@@ -15,33 +15,6 @@ app.use(express.urlencoded());
 
 app.use(userRouter);
 
-app.get("/", (req,res,next)=>{
-  res.send(`
-    <h1> Welcome to airbnb </h1>
-    <a href="/add-home">Add Home</a>
-    `);
-})
-
-
-app.get("/host/ add-home", (req,res,next)=>{
-  res.send(`
-    <h1> Register your home here sucessly: </h1>
-    <form action="/add-home" method="POST">
-    <input type="text" name="houseNmae"
-    placeholder="Enter the name of your house"/>
-    <input type="submit"/>
-    </form>
-    `);
-})
-
-app.get("/host/ add-home", (req,res,next)=>{
-  console.log(req.body);
-  res.send(`
-    <h1> Register your home here: </h1>
-    <a href="/">Go to Home</a>
-    `);
-})
-
 const server=http.createServer(app);
 
 const PORT = 3000;
---
+++ lecture 10 airbnb/router/hostRouter.js
@@ -0,0 +1,34 @@
+const express=require('express');
+const userRouter=express.Router();
+
+
+
+app.get("/", (req,res,next)=>{
+  res.send(`
+    <h1> Welcome to airbnb </h1>
+    <a href="/add-home">Add Home</a>
+    `);
+})
+
+
+hostRouter.get("/host/ add-home", (req,res,next)=>{
+  res.send(`
+    <h1> Register your home here sucessly: </h1>
+    <form action="/add-home" method="POST">
+    <input type="text" name="houseNmae"
+    placeholder="Enter the name of your house"/>
+    <input type="submit"/>
+    </form>
+    `);
+})
+
+hostRouter.post("/host/ add-home", (req,res,next)=>{
+  console.log(req.body);
+  res.send(`
+    <h1> Register your home here: </h1>
+    <a href="/">Go to Home</a>
+    `);
+})
+
+
+module.exports=hostRouter;
