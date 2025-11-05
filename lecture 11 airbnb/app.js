ebd44a9a73839be48a46063d57b489d842bc9531
raju15022004 <raju032004@gmail.com>
2025-11-05 19:53:16 +0530

    app.js

--- lecture 11 airbnb/app.js
+++ lecture 11 airbnb/app.js
@@ -15,6 +15,8 @@ app.use(express.urlencoded());
 app.use(userRouter);
 app.use(hostRouter);
 
+app.use(express.static(path.join(rootDir,'public')))
+
 app.use((req,res,next)=>{
   res.sendFile(path.join(rootDir,'../','views','404.html'));
 })
