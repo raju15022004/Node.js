4436b28affe2a48f702108db1e5dd411f0ce25fd
raju15022004 <raju032004@gmail.com>
2025-11-06 11:01:53 +0530

    userRouter.js

--- lecture 11 airbnb/router/userRouter.js
+++ lecture 11 airbnb/router/userRouter.js
@@ -1,15 +1,15 @@
 const path = require('path');
 
 const express=require('express');
-const { register } = require('module');
+// const { register } = require('module');
 const userRouter=express.Router();
-// const hostRouter=express.hostRouter();
+//  const hostRouter=express.hostRouter();
 
-
+const {registeredHomes}=require('./hostRouter');
 
 userRouter.get("/", (req,res,next)=>{
   console.log(registeredHomes);
-  res.sendFile(path.join(__dirname,'../','views','home.html'));
+  res.render('home',{registeredHomes: registeredHomes});
 });
 
 module.exports=userRouter;
